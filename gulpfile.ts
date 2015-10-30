6f1e4b2fe98a200876816cdecb49b3821a601d07
Nobby <nobby451@gmail.com>
2015-10-30 20:54:20 +0900

    Hello World的な最小構成のChrome拡張機能を作る
    manifest.jsonはYAMLから生成するので、依存関係にgulp-yamlを追加
    gulpのタスクでソースディレクトリーのルートからのディレクトリー構成を維持するためにbaseオプションを使用する
    最小限必要なmanifestをYAMLで書いてjsonに変換する

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,5 +1,13 @@
 /// <reference path="typings/gulp/gulp.d.ts" />
 
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    yaml = require('gulp-yaml');
 
-gulp.task('default', () => {});
+gulp.task('yaml', () => {
+  gulp
+    .src('src/**/*.yml', {base: 'src'})
+    .pipe(yaml())
+    .pipe(gulp.dest('dest'));
+});
+
+gulp.task('default', ['yaml']);
